afe0590d28d4750f8f3583157594dea93218a8ed
yonx <aboodshe11@gmail.com>
2025-02-26 10:47:00 +0300

    first commit

---
+++ apps/bookmodule/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+ return render(request, "bookmodule/index.html")
+def list_books(request):
+ return render(request, 'bookmodule/list_books.html')
+def viewbook(request, bookId):
+ return render(request, 'bookmodule/one_book.html')
+def aboutus(request):
+ return render(request, 'bookmodule/aboutus.html')
